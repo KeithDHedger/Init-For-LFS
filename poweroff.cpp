27d93d3f21ea054cd7146a5b557cdca2e4d33283
keithhedger <keithhedger@27563164-11e0-4794-a12e-6657db1f5c2d>
2017-03-20 12:54:42 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/StartupServices@26 27563164-11e0-4794-a12e-6657db1f5c2d

---
+++ poweroff.cpp
@@ -0,0 +1,27 @@
+#if 0
+
+#©keithhedger Fri 17 Mar 10:49:47 GMT 2017 kdhedger68713@gmail.com
+
+g++ -Wall "$0" -o poweroff||exit 1
+
+exit 0
+
+#endif
+
+#include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/reboot.h>
+#include <string.h>
+#include <linux/reboot.h>
+
+int main(int argc, char **argv)
+{
+	sync();
+	if(strcmp(argv[1],"-r")==0)
+		reboot(LINUX_REBOOT_CMD_RESTART);
+	else
+		reboot(LINUX_REBOOT_CMD_POWER_OFF);
+
+	return 0;
+}
