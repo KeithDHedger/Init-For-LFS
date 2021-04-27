d4273d5db4fc7c9380a5a89ea59f4ab242ee91aa
K D Hedger <kdhedger68713@gmail.com>
2021-04-27 20:30:28 +0100

    ...

--- poweroff.cpp
+++ poweroff.cpp
@@ -1,12 +1,22 @@
-#if 0
+/*
+ *
+ * ©K. D. Hedger. Tue 27 Apr 20:30:06 BST 2021 keithdhedger@gmail.com
 
-#©keithhedger Fri 17 Mar 10:49:47 GMT 2017 keithdhedger@gmail.com
+ * This file (poweroff.cpp) is part of Projects.
 
-g++ -Wall "$0" -o poweroff||exit 1
+ * Projects is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
 
-exit 0
+ * Projects is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-#endif
+ * You should have received a copy of the GNU General Public License
+ * along with Projects.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdio.h>
